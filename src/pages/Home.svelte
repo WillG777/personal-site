06efd409b9971872e3d27d48552bad7b8616b5fb
WillG777 <wgiorza@gmail.com>
2022-08-12 11:04:43 -0700

    added contact info

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -7,7 +7,8 @@
       <img src="self-pic.png" alt="Will Giorza" style="max-width: min(150px, 10vw)">
     </Cell>
     <Cell span={10}>
-      <p>Hello there. I'm Will Giorza, a sophomore at UC Berkeley majoring in Electrical Engineering and Computer Sciences. I've been interested in CS since middle school, and most of my experience so far is in web development. In my free time, I like hiking and reading sci-fi novels, and I'm a big fan of the Phoenix Suns and Everton Football Club.</p>
+      <p>Hello there. I'm Will Giorza, a student at UC Berkeley majoring in Electrical Engineering and Computer Sciences. I've been interested in CS since middle school, and most of my experience so far is in web development. In my free time, I like hiking and reading sci-fi novels, and I'm a big fan of the Phoenix Suns and Everton Football Club.</p>
+      <p>The best way to contact me is to message me on <a href="https://www.linkedin.com/in/will-giorza-34006020a">LinkedIn</a>.</p>
     </Cell>
   </LayoutGrid>
 </main>
