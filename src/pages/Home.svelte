e883e3d77fd6737d6d59e66324cdc2b836722d72
WillG777 <wgiorza@gmail.com>
2022-08-22 23:41:18 -0700

    changed profile picture

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -4,7 +4,7 @@
 <main>
   <LayoutGrid>
     <Cell span={2}>
-      <img src="self-pic.png" alt="Will Giorza" style="max-width: min(150px, 10vw)">
+      <img src="hiking-pic.jpeg" alt="Will Giorza" style="max-width: min(150px, 10vw)">
     </Cell>
     <Cell span={10}>
       <p>Hello there. I'm Will Giorza, a student at UC Berkeley majoring in Electrical Engineering and Computer Sciences. I've been interested in CS since middle school, and most of my experience so far is in web development. In my free time, I like hiking and reading sci-fi novels, and I'm a big fan of the Phoenix Suns and Everton Football Club.</p>
