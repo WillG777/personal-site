4561deeff321f98d494a628e86b6a15359ea577b
WillG777 <wgiorza@gmail.com>
2022-09-15 22:14:09 -0700

    added details to teaching section

--- src/pages/Teaching.svelte
+++ src/pages/Teaching.svelte
@@ -7,7 +7,7 @@
   <p>One of my favorite parts of Berkeley is the undergrad teaching experience. </p>
 
   <h3>CS 61A tutor (Fall 2022)</h3>
-  <p class="indent">As a tutor, I will be responsible for running small group sections and holding office hours.</p>
+  <p class="indent">As a tutor, I run one group section per week where I work with 5 students and go over exam or discussion problems. I also hold 5 weekly office hours where I answer students' questions on homework, projects, past exams, or general course content. Lastly, I help with other aspects of the course such as exam grading and answering questions on Ed, the course discussion forum.</p>
   <h3>CS 61A academic intern (Spring 2022)</h3>
   <p class="indent">As an AI, I worked in lab sections of 20-30 students once per week. My duties mainly consisted of helping answer debugging and conceptual questions to help students complete lab and homework assignments. I also worked with other AIs to give a mini-lecture on the Scheme language.</p>
 </main>
